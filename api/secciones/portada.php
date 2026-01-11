3f5c5c30e0328331f5769d10aa6785406ac04de2
alegonpalmaual <agp259@inlumine.ual.es>
2026-01-11 11:17:32 -0300

    Update portada.php

--- api/secciones/portada.php
+++ api/secciones/portada.php
@@ -1,20 +1,15 @@
 
+<?php
 
 /* CODIGO ORIGINAL
-/*****
-SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
-*****/
-
 $portada = [
 "titulo" => "Portada",
 "autor" => "mtorres",
 "resumen" => "El resumen maravilloso",
 ];
-?>
 */
 
 
-<?php
 // NUEVO CODIGO
 /*****
 SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
@@ -25,4 +20,4 @@ $portada = [
 "autor" => "mtorres",
 "resumen" => "Acá puedes encontrar el nuevo resumen",
 ];
-?>
+
